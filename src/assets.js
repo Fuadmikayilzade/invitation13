7e0af8ffa8ed3ad7b84571d9f774fdf8e1801c39
Fuadmikayilzade <mikayilzade5@gmail.com>
2026-08-07 13:27:08 +0400

    update table picture

--- src/assets.js
+++ src/assets.js
@@ -1,4 +1,4 @@
 export const lanternDay = "/nishan-day.jpg"
 export const lanternNight = "/nishan-night.jpg"
-export const tableSetup = "/tableSetup.jpg"
+export const tableSetup = "/tableSetup.jpeg"
 export const venue = "/venue.jpg"
